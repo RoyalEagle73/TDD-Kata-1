5ff00ff21eb3c5d1f67fee0069a036624a7fd2ba
DC086273 <deepak.chauhan@cerner.com>
2024-05-03 17:59:58 +0530

    TDD Kata 1: Added documentation

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,19 @@
+# A class to calculate sum of numbers from a given string.
 class StringCalculator
     MAX_VALUE = 1000
     DEFAULT_DILIMITER = ','
 
+    # Returns a sum of the numbers in a given string.
+    #
+    # If the string is empty, returns 0.
+    #
+    # An RuntimeError is raised if the string contains one or more negative number
+    #
+    # @param [String<numbers_in_string>] A string containing numbers with delimiters.
+    #
+    # @raise [RuntimeError] If the string contains one or more negative numbers.
+    #
+    # @return [Int] Returns the sum of numbers in string
     def add(numbers_in_string)
         ## Collect numbers from string
         numbers_list = get_numbers_from_string(numbers_in_string)
@@ -17,17 +29,23 @@ class StringCalculator
     end
 
     private
+
+    # Helper method to get numbers from a string
     def get_numbers_from_string(numbers)
         delimiter = DEFAULT_DILIMITER
+
         ## Filtering delimiter
         if numbers.start_with?("//")
             delimiter_line, numbers = numbers.lines(chomp: true)
             delimiter = delimiter_line.split("//", 2)[1].split(/\[|\]/)[1]
         end
+
         ## Remove newlines from string
         numbers.gsub("\n", "")
+
         ## Splitting numbers according to delimiter
         numbers = numbers.split(delimiter)
+
         ## Returning numbers as list
         numbers.map do |i|
              i.to_i 
